7c38f6a1173a08506324ccea54298ec02f250e4b
Jon Salenger <jbsalenger@outlook.com>
2020-02-27 18:31:08 -0500

    Update home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -8,92 +8,183 @@ Duration buttonAnim = new Duration(seconds: 5);
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+
+    /*24 is for notification bar on Android*/
+    final double itemWidth = size.width / 2;
+
+    int numberOfItemsPerRow;
+
+    if(size.width < 575 || size.height - kToolbarHeight < 575) {
+      numberOfItemsPerRow = 1;
+    } else if (size.width > 1125 || size.height - kToolbarHeight > 1125) {
+      numberOfItemsPerRow = 4;
+    } else if (size.width < 1125 || size.height - kToolbarHeight < 1125) {
+      numberOfItemsPerRow = 2;
+    } else {
+      numberOfItemsPerRow = 4;
+    }
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Jón Salenger'),
-        backgroundColor: Colors.blueGrey[800],
-      ),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
-            ),
-            Text("Hey, I'm a student. Here's a few links!", style: TextStyle(fontSize: 26), ),
-            ButtonBar(
-              alignment: MainAxisAlignment.center,
-              children: <Widget>[
-                MaterialButton(
-                  child: Text("GitHub"),
-                  animationDuration: buttonAnim,
-                  color: Colors.black,
-                  elevation: 4.0,
-                  onPressed: () {
-                    js.context.callMethod("open", ["https://www.github.com/jsalenger"]);
-                  },
-                ),
-                MaterialButton(
-                  child: Text("Twitter"),
-                  animationDuration: buttonAnim,
-                  color: Colors.blue,
-                  elevation: 4.0,
-                  onPressed: (){
-                    js.context.callMethod("open", ["https://twitter.com/_CuriosityCore_"]);
-                  },
-                )
-              ],
-            ),
-            Padding(
-              padding: EdgeInsets.fromLTRB(40, 40, 40, 15),
-            ),
-            Container(
-              alignment: Alignment.centerLeft,
-              padding: EdgeInsets.fromLTRB(30, 0, 0, 0),
-              child: Text("Hey! Why's it so empty in here?", style: TextStyle(fontSize: 20),),
-            ),
-            Divider(),
-            Container(
-              alignment: Alignment.centerRight,
-              padding: EdgeInsets.fromLTRB(5, 0, 30, 0),
-              child: Text(
-                  "Because I'm learning flutter! I don't know how to do much, but the best way to learn is by doing. I sure can say I have learned!",
-                  style: TextStyle(fontSize: 20)
+        appBar: AppBar(
+          title: const Text(
+            'Jón Salenger',
+            style: TextStyle(color: Colors.black),
+          ),
+          backgroundColor: Colors.white, // Colors.blueGrey[800],
+          elevation: 0.0,
+          bottom: PreferredSize(
+              child: Container(
+                color: Colors.deepOrangeAccent,
+                height: 4.0,
               ),
-            ),
-            Divider(),
-            Container(
-              alignment: Alignment.centerLeft,
-              padding: EdgeInsets.fromLTRB(30, 0, 0, 0),
-              child: Text("That's Great!", style: TextStyle(fontSize: 20),),
-            ),
-            Divider(),
-            Container(
-              alignment: Alignment.centerRight,
-              padding: EdgeInsets.fromLTRB(5, 0, 30, 0),
-              child: Text("Thank you!", style: TextStyle(fontSize: 20),),
-            ),
-            Padding(
-              padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
-            ),
-            Text("Also, since you've been so good, take this!", style: TextStyle(fontSize: 26), ),
-            ButtonBar(
-              alignment: MainAxisAlignment.center,
-              children: <Widget>[
-                MaterialButton(
-                  child: Text("ForgingMetal#8530"),
-                  animationDuration: buttonAnim,
-                  color: Colors.blueGrey,
-                  elevation: 4.0,
-                  onPressed: () {
-                    js.context.callMethod("open", ["https://www.discordapp.com"]);
-                  },
+              preferredSize: Size.fromHeight(4.0)),
+        ),
+        body: GridView.count(
+            crossAxisCount: numberOfItemsPerRow,
+            padding: EdgeInsets.all(16.0),
+            childAspectRatio: (itemWidth / itemWidth),
+            // TODO: Build a grid of cards (102)
+            children: <Widget>[
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                shadowColor: Colors.deepOrange,
+                clipBehavior: Clip.antiAlias,
+                elevation: 8.0,
+                child: Scaffold(
+                  bottomNavigationBar: ButtonBar(
+                    alignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      OutlineButton(
+                        child: Text("Twitter"),
+                        onPressed: () {
+                          js.context.callMethod(
+                              "open", ["https://twitter.com/_CuriosityCore_"]);
+                        },
+                        // shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0))
+                      )
+                    ],
+                  ),
+                  body: Padding(
+                    padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          'Twitter',
+                          style: TextStyle(fontSize: 26.0),
+                        ),
+                        SizedBox(height: 8.0),
+                        Text(
+                            'Here\'s a link to my twitter. If you\'d like to follow me that\'s cool 👍. I make funny tweets'),
+                        Text(
+                          'sometimes.',
+                          style: TextStyle(color: Colors.black26),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
-              ],
-            ),
-          ],
-        )
-      ),
-    );
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                shadowColor: Colors.deepOrange,
+                clipBehavior: Clip.antiAlias,
+                elevation: 8.0,
+                child: Scaffold(
+                  bottomNavigationBar: ButtonBar(
+                    alignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      OutlineButton(
+                        splashColor: Color.fromARGB(50, 0, 0, 0),
+                        textColor: Colors.black,
+                        highlightedBorderColor: Colors.black,
+                        child: Text("GitHub"),
+                        onPressed: () {
+                          js.context.callMethod(
+                              "open", ["https://github.com/jsalenger"]);
+                        },
+                        // shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0))
+                      )
+                    ],
+                  ),
+                  body: Padding(
+                    padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          'GitHub',
+                          style: TextStyle(fontSize: 26.0),
+                        ),
+                        SizedBox(height: 8.0),
+                        Text(
+                            'I make stuff on github sometimes its cool.'),
+                        Text(
+                          'sometimes.',
+                          style: TextStyle(color: Colors.black26),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                shadowColor: Colors.deepOrange,
+                clipBehavior: Clip.antiAlias,
+                elevation: 8.0,
+                child: Scaffold(
+                  bottomNavigationBar: ButtonBar(
+                    alignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      OutlineButton(
+                        textColor: Colors.blueGrey,
+                        highlightedBorderColor: Colors.blueGrey,
+                        child: Text("Vik"),
+                        onPressed: () {
+                          js.context.callMethod(
+                              "open", ["vik.jbsalenger.com"]);
+                        },
+                        // shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0))
+                      )
+                    ],
+                  ),
+                  body: Padding(
+                    padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          'Vik.fyi',
+                          style: TextStyle(fontSize: 26.0),
+                        ),
+                        SizedBox(height: 8.0),
+                        Text(
+                            'Previously hosted on the doman \'vik.fyi\' it has become kind of a meme. '
+                                'It is a basic image sharing platform built off django. '
+                                'It was my first foreé ( I think that\'s right ) into web development. '
+                                'It\'s usually up'
+                        ),
+                        Text(
+                          'usually.',
+                          style: TextStyle(color: Colors.black26),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ])
+        );
   }
 }
 
+BoxDecoration myBoxDecoration() {
+  return BoxDecoration(
+    border: Border.all(width: 3.0),
+    borderRadius:
+        BorderRadius.all(Radius.circular(5.0) //         <--- border radius here
+            ),
+  );
+}
